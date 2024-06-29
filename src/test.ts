deb485405fc886b69c3fbe27319008b919d3d053
nickmura <nickmuradev@gmail.com>
2024-06-29 02:03:45 -0600

    changed test for forNodejs() in test, edited sui-kit-plugins pkg

--- src/test.ts
+++ src/test.ts
@@ -1,25 +1,56 @@
-import { SuiKit, SuiTxBlock } from '@scallop-io/sui-kit';
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+import { SuiKit, SuiTxBlock, } from '@scallop-io/sui-kit';
 // import plugins, they will dynamically register themselves to SuiKit
 import '@scallop-io/sui-kit-plugins';
 import * as process from "process";
+import { mnemonicToSeedHex, toB64 } from '@mysten/sui.js';
 
-let suiKit = new SuiKit({secretKey: String(process.env.SHINAMI_SECRET_KEY)});
+// import { getFullnodeUrl, SuiClient } from '@mysten/sui.js/client';
+// import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
+// import { TransactionBlock } from '@mysten/sui.js/transactions';
+
+const secret_key =  process.env.SECRET_KEY;
+const gas_access_key = process.env.SHINAMI_GAS_ACCESS_KEY;
+
+
+
+let suiKit = new SuiKit({mnemonics: secret_key, networkType: 'testnet'});
 // init Shinami gas sponsor before using it
-suiKit.initShinamiGasSponsor(String(process.env.SHINAMI_GAS_ACCESS_KEY));
+suiKit.initShinamiGasSponsor(gas_access_key ?? ``);
+
 
 /**
  * This is an example of using sponsored transaction plugin in nodejs.
  */
-async function forNodejs() {
-  // Create a transaction
-  const tx = new SuiTxBlock();
-  tx.transferObjects(['<obj_id>'], '<sender_address>');
 
+let Package = `0xd7e2d8d40e227b967f1cd6c554a14040ff8af1f43f382fe9678febc4a3847673`
+let JSONPackage = {"Package":`${Package}`}
+//const MoveEventType = '<PACKAGE_ID>::<MODULE_NAME>::<METHOD_NAME>';
+const MoveEventType = `0x2::coin::Coin<0xd7e2d8d40e227b967f1cd6c554a14040ff8af1f43f382fe9678febc4a3847673::simple_token::SIMPLE_TOKEN>`;
+
+
+async function forNodejs() { 
+
+  // Create a transaction
+  const tx = new SuiTxBlock(); //@ts-ignore
+  tx.moveCall({
+    // object IDs must be wrapped in moveCall arguments
+    target: "0xfa0e78030bd16672174c2d6cc4cd5d1d1423d03c28a74909b2a148eda8bcca16::clock::access",
+    
+
+  })
+  //@ts-ignore
+    tx.transferObjects([tx.object('0x6')], `0xe7db53637032a84492a399445392f9c8a1a3863bcd0b66503f5caf4211ec221d`)
+    
   const gasBudget = 10 ** 9;
-  // Sponsor the transaction, and send it
   const res = await suiKit.signAndSendShinamiSponsoredTxn(tx, gasBudget);
-  return res;
-}
+  console.log(res)
+  // Sponsor the transaction, and send it
+
+} forNodejs()
 
 /**
  * This is an example of using sponsored transaction plugin in browser.
@@ -39,11 +70,17 @@ async function forBrowser() {
   const userSignature = await getUserSignatureFromWallet(txBytes);
   // Send the signed sponsored transaction
   const res = await suiKit.sendShinamiSponsoredTxn(sponsoredTx, userSignature);
-  
   return res;
+  
 }
 
 
 async function getUserSignatureFromWallet(tx:string) {
+
     return ''
+}
+
+
+async function invokeTransaction() {
+
 }
